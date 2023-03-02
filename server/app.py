2f521b89ff7b6862b8ad22a75ebc80329c1fae79
DominicFinn <dom.finn@gmail.com>
2023-03-02 15:00:06 +0000

    python server

---
+++ server/app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask, jsonify
+import perks
+
+app = Flask(__name__) # create an instance of the Flask class
+
+
+# defining a route
+@app.route("/", methods=['GET', 'POST', 'PUT']) # decorator
+def home(): # route handler function
+    perkIds = perks.perkIds()
+    
+    response = jsonify({'perks': perkIds})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+# running the server
+app.run(debug = True) # to allow for debugging and auto-reload
---
+++ server/perks.py
@@ -0,0 +1,5 @@
+
+def perkIds():
+     return [1, 2, 3, 4, 5]
+
+   
